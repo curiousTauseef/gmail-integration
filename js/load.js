4143a1efc709906dfa1796f6cad0077a01345324
Tejas Manohar <me@tejasmanohar.com>
2014-08-04 13:27:05 -0500

    fix gmail variable

--- js/load.js
+++ js/load.js
@@ -1,12 +1,13 @@
 window.onload = function () {
 
   var jq = document.createElement('script');
-  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
-  document.getElementsByTagName('body')[0].appendChild(jq)
+  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+  document.getElementsByTagName('body')[0].appendChild(jq);
 
   var sm = document.createElement('script');
   sm.src = "https://rawgit.com/KartikTalwar/gmail.js/master/gmail.min.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
+  var gmail = Gmail();
 
   var sm = document.createElement('script');
   sm.src = "https://rawgit.com/tejas-manohar/gmail-integration/master/js/gm_inject.js";
